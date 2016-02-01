c74fb4adac5bc620e67d4400db825aa2f15b8f94
bcui <bcui@me.com>
2016-02-01 10:06:15 +0800

    add appComponent

---
+++ app/Components/appComponent/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/Components/appComponent/app.Component.html'
+})
+export class AppComponent { 
+    
+}
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './Components/appComponent/app.component'
+
+bootstrap(AppComponent);
